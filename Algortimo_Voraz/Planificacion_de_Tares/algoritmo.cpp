9de2a91e9d1c8887d26ee9ee6767f2dbcbba914c
Braulio Maldonado Casilla <87953250+ShyniYT@users.noreply.github.com>
2023-07-05 09:54:26 -0500

    Update algoritmo.cpp

--- Algortimo_Voraz/Planificacion_de_Tares/algoritmo.cpp
+++ Algortimo_Voraz/Planificacion_de_Tares/algoritmo.cpp
@@ -4,38 +4,43 @@
 
 void scheduleTasks(const std::vector<int>& b, const std::vector<int>& d) {
     int n = b.size();
-    std::vector<std::pair<int, int>> tareas;
+    std::vector<std::pair<int, std::pair<int, int>>> tareas;
 
     for (int i = 0; i < n; i++) {
-        tareas.push_back(std::make_pair(i + 1, b[i]));
+        tareas.push_back(std::make_pair(i + 1, std::make_pair(b[i], d[i])));
     }
 
-    std::sort(tareas.begin(), tareas.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
-        return a.second > b.second;
+    std::sort(tareas.begin(), tareas.end(), [](const std::pair<int, std::pair<int, int>>& a, const std::pair<int, std::pair<int, int>>& b) {
+        if (a.second.first != b.second.first) {
+            return a.second.first > b.second.first;
+        }
+        else {
+            return a.first < b.first;
+        }
         });
 
-    std::vector<std::pair<int, int>> solucion;
+    std::vector<std::pair<int, std::pair<int, int>>> solucion;
     std::vector<bool> disponibilidad(n, true);
 
-    for (const std::pair<int, int>& data : tareas) {
-        int p = d[data.first - 1];
+    for (const std::pair<int, std::pair<int, int>>& data : tareas) {
+        int p = data.second.second;
 
         for (int i = std::min(n, p) - 1; i >= 0; i--) {
             if (disponibilidad[i]) {
-                solucion.push_back(data);
+                solucion.push_back(std::make_pair(data.first, std::make_pair(data.second.first, i + 1)));
                 disponibilidad[i] = false;
                 break;
             }
         }
     }
 
-    std::sort(solucion.begin(), solucion.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
-        return a.first < b.first;
+    std::sort(solucion.begin(), solucion.end(), [](const std::pair<int, std::pair<int, int>>& a, const std::pair<int, std::pair<int, int>>& b) {
+        return a.second.second < b.second.second;
         });
 
     std::cout << "Solucion Optima: " << std::endl;
-    for (const std::pair<int, int>& task : solucion) {
-        std::cout << task.first << " - " << task.second << std::endl;
+    for (const std::pair<int, std::pair<int, int>>& task : solucion) {
+        std::cout << task.first << " - " << task.second.first << " (Plazo: " << task.second.second << " dias)" << std::endl;
     }
 }
 
