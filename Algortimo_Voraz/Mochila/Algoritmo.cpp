ee1c1645d3d27901b1cb8c27c79f3673d793aa24
Braulio Maldonado Casilla <87953250+ShyniYT@users.noreply.github.com>
2023-07-05 09:39:28 -0500

    Update Algoritmo.cpp

--- Algortimo_Voraz/Mochila/Algoritmo.cpp
+++ Algortimo_Voraz/Mochila/Algoritmo.cpp
@@ -12,9 +12,9 @@ void Mochila(int M, std::vector<int> b, std::vector<int> p, std::vector<int>& X)
         int mejorObjeto = -1;
         double mejorRatio = 0.0;
         for (int i = 0; i < n; i++) {
-            if (X[i] == 0 && (mejorObjeto == -1 || (double)p[i] / b[i] > mejorRatio)) {
+            if (X[i] == 0 && (mejorObjeto == -1 || (double)b[i] / p[i] > mejorRatio)) {
                 mejorObjeto = i;
-                mejorRatio = (double)p[i] / b[i];
+                mejorRatio = (double)b[i] / p[i];
             }
         }
         if (mejorObjeto == -1) {
@@ -26,9 +26,9 @@ void Mochila(int M, std::vector<int> b, std::vector<int> p, std::vector<int>& X)
             beneficioTotal += b[mejorObjeto];
         }
         else {
-            X[mejorObjeto] = (M - pesoAct) / p[mejorObjeto];
+            X[mejorObjeto] = 1;
             pesoAct = M;
-            beneficioTotal += X[mejorObjeto] * b[mejorObjeto];
+            beneficioTotal += b[mejorObjeto];
         }
     }
     std::cout << "Beneficio total: " << beneficioTotal << std::endl;
@@ -41,7 +41,7 @@ int main() {
     std::vector<int> X(b.size());
     Mochila(M, b, p, X);
     for (int i = 0; i < b.size(); i++) {
-        std::cout << "Objeto " << i+1 << ": " << X[i] << std::endl;
+        std::cout << "Objeto " << i + 1 << ": " << X[i] << std::endl;
     }
     return 0;
 }
